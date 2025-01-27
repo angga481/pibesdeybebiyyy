7b3c1f4cdd86d93bcd18f94b3f8cc145cc642aed
angga481 <xxhaiipapii@gmail.com>
2025-01-27 23:25:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 5000);
+showSlide(currentIndex);
